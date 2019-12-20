06e38b1be9abfab471eb8bdaf81ed791867fd3fc
Uchendu Precious <uchendubozz@gmail.com>
2019-12-20 11:36:49 +0100

    added phone to the user registration payload

--- routes/user.route.js
+++ routes/user.route.js
@@ -15,11 +15,12 @@ const router = express.Router();
     let lastname = req.body.lastname;
     let email = req.body.email;
     let password = req.body.password;
+    let phone = req.body.phone
     let salt = 10;
     const { error } = validateUserCreate({firstname,lastname,email,password}); 
     if (error) return res.status(400).send(error.details[0].message);
     await bcrypt.hash(password, salt, (err, hash) => {
-      var sql = `INSERT INTO users (firstname, lastname, email, password) VALUES ("${firstname}", "${lastname}", "${email}", "${hash}")`;
+      var sql = `INSERT INTO users (firstname, lastname, email, password, phone) VALUES ("${firstname}", "${lastname}", "${email}", "${hash}, "${phone}")`;
       db.query(sql, function(err, result) {
         if (err) {
           res.status(500).send({ error: "An Error Has Occurred"  });
